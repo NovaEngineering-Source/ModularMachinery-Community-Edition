6f1505b305ec10156ff7adeda225408bb67cfbb2
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-10-18 22:34:01 +0900

    Update ModularUI to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -168,7 +168,7 @@ dependencies {
     compileOnly(rfg.deobf("curse.maven:mantle-74924:2713386"))
     implementation(rfg.deobf("curse.maven:tx-loader-706505:4515357"))
     implementation(rfg.deobf("curse.maven:applied-energistics-2-223794:2747063"))
-    implementation("curse.maven:modularui-624243:4779301-sources-4779302")
+    implementation("curse.maven:modularui-624243:4786371-sources-4786373")
 
 //    compileOnly(rfg.deobf("curse.maven:gregtech-293327:3266351"))
     implementation(rfg.deobf("curse.maven:gregtech-ce-unofficial-557242:4799055"))
