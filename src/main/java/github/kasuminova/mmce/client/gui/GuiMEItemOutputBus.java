63569f926b01a66959884f6afd31872dacee38f3
ArshdipKhinda <akhinda16@gmail.com>
2025-11-05 23:20:27 -0800

    Configure stack size button moved to the left

--- src/main/java/github/kasuminova/mmce/client/gui/GuiMEItemOutputBus.java
+++ src/main/java/github/kasuminova/mmce/client/gui/GuiMEItemOutputBus.java
@@ -1,6 +1,8 @@
 package github.kasuminova.mmce.client.gui;
 
-import appeng.client.gui.widgets.GuiTabButton;
+import appeng.api.config.ActionItems;
+import appeng.api.config.Settings;
+import appeng.client.gui.widgets.GuiImgButton;
 import appeng.core.localization.GuiText;
 import github.kasuminova.mmce.common.container.ContainerMEItemOutputBus;
 import github.kasuminova.mmce.common.network.PktSwitchGuiMEOutputBus;
@@ -13,12 +15,13 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
 import java.io.IOException;
+import java.util.List;
 
 public class GuiMEItemOutputBus extends GuiMEItemBus {
     // For convenience, the Sky Chest resource was used :P
     private static final ResourceLocation TEXTURES_OUTPUT_BUS = new ResourceLocation("appliedenergistics2", "textures/guis/skychest.png");
 
-    private GuiTabButton stackSizeBtn;
+    private CustomStackSizeButton stackSizeBtn;
     private final MEItemOutputBus outputBus;
 
     public GuiMEItemOutputBus(final MEItemOutputBus te, final EntityPlayer player) {
@@ -32,14 +35,12 @@ public class GuiMEItemOutputBus extends GuiMEItemBus {
         super.initGui();
 
         // Add wrench button for stack size configuration
-        // Icon ID: 2 + 4 * 16 = 66 (wrench icon in AE2's states.png texture)
-        this.buttonList.add(this.stackSizeBtn = new GuiTabButton(
-                this.guiLeft + 154,
-                this.guiTop,
-                2 + 4 * 16,
-                "Stack Size",
-                this.itemRender
-        ));
+        // Position: Left side, matching AE2's Partition Storage button position
+        this.stackSizeBtn = new CustomStackSizeButton(
+                this.guiLeft - 18,          // Left side of GUI
+                this.guiTop + 8             // Top position
+        );
+        this.buttonList.add(this.stackSizeBtn);
     }
 
     @Override
@@ -65,6 +66,21 @@ public class GuiMEItemOutputBus extends GuiMEItemBus {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(TEXTURES_OUTPUT_BUS);
         this.drawTexturedModalRect(offsetX, offsetY, 0, 0, this.xSize, this.ySize);
+    }
+
+    /**
+     * Custom button that extends GuiImgButton but overrides the tooltip message
+     */
+    private static class CustomStackSizeButton extends GuiImgButton {
+        public CustomStackSizeButton(int x, int y) {
+            super(x, y, Settings.ACTIONS, ActionItems.WRENCH);
+        }
+
+        @Override
+        public String getMessage() {
+            // Return our custom tooltip instead of AE2's default
+            return "Configure Stack Size";
+        }
     }
 
 }
